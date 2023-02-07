d4eebc56b3f7f499fb4efc8f33f80f6c5e8a1eab
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-07 10:20:21 +0200

    feat: add fonts and colors to template: 20min

--- composables/fonts.ts
+++ composables/fonts.ts
@@ -1,4 +1,28 @@
-const fontsMetaList = []
+const fontsMetaList = [
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Gerlomi.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Manrope-VariableFont_wght.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/RecifeDisplay-RegularItalic.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+]
 export const useFonts = () => {
   useHead({
     link: [...fontsMetaList],
