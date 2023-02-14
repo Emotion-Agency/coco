29ea37f71e9611541d113bf740722819f22beff2
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-14 12:22:34 +0200

    feature: update filter

--- components/Filter.vue
+++ components/Filter.vue
@@ -2,15 +2,19 @@
 const categories = [
   {
     text: 'All',
+    inputId: 'all',
   },
   {
     text: 'Category 1',
+    inputId: 'category1',
   },
   {
     text: 'Category 2',
+    inputId: 'category2',
   },
   {
     text: 'Category 3',
+    inputId: 'category3',
   },
 ]
 
@@ -38,6 +42,29 @@ onMounted(async () => {
         <button class="desktop-filter__btn">{{ el.text }}</button>
       </li>
     </ul>
-    <div v-else class="mobile-filter"></div>
+    <div v-else class="mobile-filter">
+      <button class="mobile-filter__button">
+        Choose category
+        <IconsFilterMark />
+      </button>
+      <ul class="mobile-filter__list">
+        <li
+          v-for="(el, idx) in categories"
+          :key="idx"
+          class="mobile-filter__li"
+        >
+          <input
+            :id="el.inputId"
+            class="mobile-filter__radio"
+            type="radio"
+            name="filter-radios"
+            checked
+          />
+          <label class="mobile-filter__text" :for="el.inputId">
+            {{ el.text }}
+          </label>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
