3940eef0fe58928900e6000ca7cc0020fed8985c
letmewin22 <lkosteckiy5@gmail.com>
2023-03-06 12:11:50 +0200

    feat: add sitemap and robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   css: ['@/assets/styles/index.scss'],
 
   modules: [
+    'nuxt-simple-robots',
     [
       '@storyblok/nuxt',
 
@@ -30,7 +31,13 @@ export default defineNuxtConfig({
       IMGIX_DOMAIN: process.env.IMGIX_DOMAIN,
       IMGIX_KEY: process.env.IMGIX_KEY,
       BACKEND_URL: process.env.BACKEND_URL,
+      siteUrl:
+        process.env.NUXT_PUBLIC_SITE_URL || 'https://www.relivebycoco.com',
     },
+  },
+
+  robots: {
+    sitemap: '/sitemap.xml',
   },
 
   vite: {
