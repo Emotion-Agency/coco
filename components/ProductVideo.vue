b27e1cbbb8286854fd1db9f9b6e6c82b5ab3a5ea
letmewin22 <lkosteckiy5@gmail.com>
2023-03-06 18:30:54 +0200

    feat: improve video btn

--- components/ProductVideo.vue
+++ components/ProductVideo.vue
@@ -6,6 +6,7 @@ interface iProps {
 }
 
 const isPlaying = ref(false)
+const isBtnVisible = ref(true)
 const $video = ref<HTMLVideoElement>(null)
 
 const props = defineProps<iProps>()
@@ -18,13 +19,20 @@ const playHandler = () => {
   isPlaying.value = !isPlaying.value
 
   isPlaying.value ? $video.value.play() : $video.value.pause()
+
+  isBtnVisible.value = !isPlaying.value
 }
 </script>
 
 <template>
-  <div class="product-video">
+  <div
+    class="product-video"
+    @mouseenter="isBtnVisible = true"
+    @mouseleave="isPlaying && (isBtnVisible = false)"
+  >
     <button
       class="product-video__play-btn"
+      :class="isBtnVisible && 'product-video__play-btn--visible'"
       aria-label="Play video button"
       @click="playHandler"
     >
@@ -34,6 +42,7 @@ const playHandler = () => {
     <video
       ref="$video"
       :src="url"
+      loop
       muted
       :class="videoClass"
       :controls="isPlaying"
