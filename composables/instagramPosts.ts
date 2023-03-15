e1ae2959c83ae3161ba88a14ab0a90a1806ecb10
letmewin22 <lkosteckiy5@gmail.com>
2023-03-15 16:57:02 +0200

    feat: update insta script

--- composables/instagramPosts.ts
+++ composables/instagramPosts.ts
@@ -1,26 +1,28 @@
-import { AxiosResponse } from 'axios'
 import InstagramService from '~~/api/instagram-service'
 import { iInstaPost } from '~~/types'
 
 export const useInstagramPosts = () => {
   const posts = useState<iInstaPost[]>('instagramPosts', () => [])
 
-  const getPosts = async (links: string[]) => {
+  const getPosts = (links: string[]) => {
+    posts.value = links.map(link => ({
+      postUrl: link,
+      postMedia: '/images/thumb.jpg',
+    }))
+
     try {
-      const promises = links
-        .map(postId => InstagramService.getPost(postId))
-        .slice(0, 10)
+      links.forEach((link, idx) => {
+        if (idx >= 10) {
+          return
+        }
 
-      const postsData = (await Promise.all(
-        promises
-      )) as unknown as AxiosResponse<iInstaPost>[]
-
-      posts.value = postsData.map(pd => ({
-        ...pd.data,
-        postMedia: pd.data.postMedia,
-      }))
-
-      console.log(posts.value)
+        InstagramService.getPost(link).then(postData => {
+          posts.value[idx] = {
+            ...postData.data,
+            postMedia: postData.data.postMedia,
+          }
+        })
+      })
     } catch (error) {
       posts.value = links.map(link => ({
         postUrl: link,
