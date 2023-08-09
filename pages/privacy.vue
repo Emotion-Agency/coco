4bf175461ddcc1a2cff02be9ee0df1a6421fd9e9
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-08-09 12:02:40 +0300

    feat: add returns page with adaptive

---
+++ pages/privacy.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { pageTransition } from '~~/assets/scripts/transition'
+
+definePageMeta({
+  pageTransition,
+})
+</script>
+
+<template>
+  <main>
+    <section class="privacy">privacy</section>
+  </main>
+</template>
